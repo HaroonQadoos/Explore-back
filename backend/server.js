b579b17085dc8d247c268c294cedf31ef5e51bd6
Haroon Qadoos <haroonriaz034@gmail.com>
2026-01-07 02:06:42 +0500

    once again

--- backend/server.js
+++ backend/server.js
@@ -48,5 +48,5 @@ app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// module.exports = app;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
