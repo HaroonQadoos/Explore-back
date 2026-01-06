bbd5af5aae4183238b5fbf3f7a2d460426a03337
Haroon Qadoos <haroonriaz034@gmail.com>
2026-01-07 03:17:37 +0500

    updated again and again

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(
   cors({
     origin: [
       "https://explore-front-aznp.vercel.app",
-      "https://explore-dash-l8vr.vercel.app",
+      "https://explore-dash-a3tm.vercel.app",
     ], // frontend origin
     credentials: true, // allow cookies
   })
