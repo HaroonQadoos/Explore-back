af45eb45e5a313c52d0273b4df729d82d3a875e9
Haroon Qadoos <haroonriaz034@gmail.com>
2026-01-07 22:52:03 +0500

    updated streamify

--- backend/controllers/uploadController.js
+++ backend/controllers/uploadController.js
@@ -1,7 +1,7 @@
 const cloudinary = require("../config/cloudinary");
 const multer = require("multer");
+const streamifier = require("streamifier");
 
-// multer memory storage
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
@@ -9,11 +9,9 @@ exports.uploadImage = [
   upload.single("image"),
   async (req, res) => {
     try {
-      if (!req.file) {
+      if (!req.file)
         return res.status(400).json({ message: "No file uploaded" });
-      }
 
-      // Wrap upload_stream in a Promise to use async/await
       const uploadToCloudinary = (fileBuffer) =>
         new Promise((resolve, reject) => {
           const stream = cloudinary.uploader.upload_stream(
@@ -23,11 +21,10 @@ exports.uploadImage = [
               else resolve(result);
             }
           );
-          stream.end(fileBuffer);
+          streamifier.createReadStream(fileBuffer).pipe(stream);
         });
 
       const result = await uploadToCloudinary(req.file.buffer);
-
       res.status(200).json({ url: result.secure_url });
     } catch (err) {
       console.error(err);
